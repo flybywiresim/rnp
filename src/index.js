116e77acc230b4035fb14b724c45993848545708
Gus Caplan <me@gus.host>
2020-11-25 16:31:00 -0600

    100% coverage

--- src/index.js
+++ src/index.js
@@ -4,29 +4,30 @@ const { Parser } = require('./parser');
 const { Assembler } = require('./assembler');
 
 function translate(source, specifier) {
-    const ast = Parser.parse(source, specifier);
-    const out = Assembler.assemble(ast, source, specifier);
-    return out;
+  const ast = Parser.parse(source, specifier);
+  const out = Assembler.assemble(ast, source, specifier);
+  return out;
 }
 
 module.exports = {
-    translate,
+  translate,
 };
 
+/* istanbul ignore next */
 if (require.main === module) {
-    /* eslint-disable global-require */
-    const util = require('util');
-    const repl = require('repl');
+  /* eslint-disable global-require */
+  const util = require('util');
+  const repl = require('repl');
 
-    repl.start({
-        prompt: '> ',
-        eval(source, c, f, cb) {
-            try {
-                cb(null, translate(source, '(repl)'));
-            } catch (e) {
-                cb(e, null);
-            }
-        },
-        writer: (v) => (typeof v === 'string' ? v : util.inspect(v)),
-    });
+  repl.start({
+    prompt: '> ',
+    eval(source, c, f, cb) {
+      try {
+        cb(null, translate(source, '(repl)'));
+      } catch (e) {
+        cb(e, null);
+      }
+    },
+    writer: (v) => (typeof v === 'string' ? v : util.inspect(v)),
+  });
 }
--- test/index.js
+++ test/index.js
@@ -2,23 +2,44 @@
 
 const fs = require('fs');
 const path = require('path');
-const assert = require('assert');
+const tap = require('tap');
 const { translate } = require('..');
 
 function* readdir(d) {
-    for (const dirent of fs.readdirSync(d, { withFileTypes: true })) {
-        const resolved = path.join(d, dirent.name);
-        if (dirent.isDirectory()) {
-            yield* readdir(resolved);
-        } else {
-            yield resolved;
-        }
+  for (const dirent of fs.readdirSync(d, { withFileTypes: true })) {
+    const resolved = path.join(d, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* readdir(resolved);
+    } else {
+      yield resolved;
     }
+  }
 }
 
-for (const test of readdir(path.join(__dirname, 'cases'))) {
-    const source = fs.readFileSync(test, 'utf8');
-    const [input, output] = source.split('---');
+for (const filename of readdir(path.join(__dirname, 'pass'))) {
+  const test = path.relative(__dirname, filename);
+  tap.test(test, (t) => { // eslint-disable-line no-loop-func
+    const [source, expectedRaw] = fs.readFileSync(filename, 'utf8').split('---');
     // for the moment rnp emits a single line of code, so don't worry about newlines and such
-    assert.strictEqual(translate(input.trim(), test), output.trim().replace(/\s*\n\s*/g, ' '));
+    const actual = translate(source, test);
+    const expected = expectedRaw
+      .trim()
+      .replace(/\s*\n\s*/g, ' ');
+    t.equal(actual, expected);
+    t.end();
+  });
+}
+
+for (const filename of readdir(path.join(__dirname, 'fail'))) {
+  const test = path.relative(__dirname, filename);
+  tap.test(test, (t) => { // eslint-disable-line no-loop-func
+    const [source, expected] = fs.readFileSync(filename, 'utf8').split('---\n');
+    try {
+      translate(source, test);
+      t.fail();
+    } catch (e) {
+      t.equal(e.stack.split('    at')[0], expected);
+    }
+    t.end();
+  });
 }
