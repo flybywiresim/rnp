221ea57e822657b38149698c14eca66ad65e38c3
Gus Caplan <me@gus.host>
2020-12-21 11:49:09 -0600

    optimized cm mode

--- site/main.mjs
+++ site/main.mjs
@@ -3,32 +3,79 @@
 
 import rnp from './rnp.mjs';
 
-CodeMirror.defineSimpleMode('rnp', {
-  start: [
-    { regex: /(?:if|else|let|alias|macro|import|export|from)\b/, token: 'keyword' },
-    { regex: /(?:true|false)\b|\(.:.+?\)/, token: 'atom' },
-    { regex: /\$\p{ID_Continue}*/u, token: 'variable-2' },
-    { regex: /\p{ID_Start}\p{ID_Continue}*/u, token: 'variable' },
-    { regex: /'(?:[^\\]|\\.)*?(?:'|$)/, token: 'string' },
-    { regex: /-?0x[a-f\d]+|-?0b[01]+|-?(?:\.\d+|\d+\.?\d*)(?:e-?\d+)?/i, token: 'number' },
-    { regex: /[-+/*=<>!]+/, token: 'operator' },
-    { regex: /{/, indent: true },
-    { regex: /}/, dedent: true },
-    { regex: /#\*/, token: 'comment', push: 'comment' },
-    { regex: /#(?!\*).*/, token: 'comment' },
-  ],
-  comment: [
-    { regex: /#\*/, token: 'comment', push: 'comment' },
-    { regex: /(.|\n)*?\*#/, token: 'comment', pop: true },
-    { regex: /((?!#\*).|\n)*/, token: 'comment' },
-  ],
-  meta: {
-    dontIndentStates: ['comment'],
-    lineComment: '#',
-    fold: 'brace',
-    closeBrackets: "(){}''",
+CodeMirror.defineMode('rnp', (config) => ({
+  startState() {
+    return {
+      blockComment: 0,
+      indent: [],
+    };
   },
-});
+  token(stream, state) {
+    if (stream.match('#*')) {
+      state.blockComment += 1;
+      return 'comment';
+    }
+    if (stream.match('*#')) {
+      state.blockComment -= 1;
+      return 'comment';
+    }
+    if (state.blockComment) {
+      stream.next();
+      return 'comment';
+    }
+    if (stream.eat('#')) {
+      stream.skipToEnd();
+      return 'comment';
+    }
+    if (stream.eatSpace()) {
+      return null;
+    }
+    if (stream.match(/(?:if|else|let|alias|macro|import|export|from)\b/)) {
+      return 'keyword';
+    }
+    if (stream.match(/(?:true|false)\b|\(.:.+?\)/)) {
+      return 'atom';
+    }
+    if (stream.match(/(?:[-+/*=<>!]+)|(?:(?:and|or)\b)/)) {
+      return 'operator';
+    }
+    if (stream.match(/\$\p{ID_Continue}*/u)) {
+      return 'variable-2';
+    }
+    if (stream.match(/\p{ID_Start}\p{ID_Continue}*/u)) {
+      return 'variable';
+    }
+    if (stream.match(/'(?:[^\\]|\\.)*?(?:'|$)/)) {
+      return 'string';
+    }
+    if (stream.match(/-?0x[a-f\d]+|-?0b[01]+|-?(?:\.\d+|\d+\.?\d*)(?:e-?\d+)?/i)) {
+      return 'number';
+    }
+    if (stream.eat(/{|\(/)) {
+      state.indent.push(stream.indentation() + config.indentUnit);
+      return null;
+    }
+    if (stream.eat(/\)|}/)) {
+      state.indent.pop();
+      return null;
+    }
+    stream.next();
+    return null;
+  },
+  indent(state, textAfter) {
+    let pos = state.indent.length - 1;
+    for (let i = 0; i < textAfter.length; i += 1) {
+      if (textAfter[i] === '}' || textAfter[i] === ')') {
+        pos -= 1;
+      }
+    }
+    return pos < 0 ? 0 : state.indent[pos];
+  },
+  lineComment: '#',
+  blockCommentStart: '#*',
+  blockCommendEnd: '*#',
+  closeBrackets: '(){}\'\'',
+}));
 
 let cmErrors = [];
 CodeMirror.registerHelper('lint', 'rnp', () => cmErrors);
